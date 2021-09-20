62cc566d3b6922869fb7da25f0252472626504d4
khalilbenyahyaten <2300@holbertonschool.com>
2021-09-20 14:57:29 +0200

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains one function: canUnlockAll()"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    keysList = [0]
+    for key in keysList:
+        for k in boxes[key]:
+            if k not in keysList and k < len(boxes):
+                keysList.append(k)
+    for i in range(len(boxes)):
+        if i not in keysList:
+            return False
+    return True
+    
