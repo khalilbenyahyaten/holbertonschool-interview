9b0c32ac0bd169aa489251169bb87c09fbbe6adc
khalilbenyahyaten <2300@holbertonschool.com>
2021-09-22 16:36:49 +0200

    header file

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -21,4 +21,5 @@ typedef struct binary_tree_s
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
 #endif
