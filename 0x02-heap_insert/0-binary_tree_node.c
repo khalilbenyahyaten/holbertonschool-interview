7b4e59558bdab28951f373822e5a0c47096db067
khalilbenyahyaten <2300@holbertonschool.com>
2021-09-22 16:31:35 +0200

    binary tree node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,10 +8,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
-{
-n = malloc(sizeof(binary_tree_t));
+node = malloc(sizeof(binary_tree_t));
 if (node == NULL)
+{
 return (0);
+}
 node->parent = parent;
 node->n = value;
 node->left = NULL;
