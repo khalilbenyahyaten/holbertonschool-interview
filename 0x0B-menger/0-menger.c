2cd804324e91af387074831a6ea2cc1e207a555e
khalilbenyahyaten <2300@holbertonschool.com>
2021-10-06 18:05:37 +0200

    menger sponge

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,39 +1,34 @@
 #include "menger.h"
-/**
- * addSpace - Checks whether cell of grid needs to be a space
- * @i: row
- * @j: column
- * Return: 1 if cell is a space, 0 if it isn't.
- */
-int addSpace(int i, int j)
-{
-	while (j && i)
-	{
-		if (i % 3 == 1 && j % 3 == 1)
-			return (1);
-		i = i / 3, j = j / 3;
-	}
-	return (0);
-}
 
 /**
- * menger - draws a 2D Menger Sponge
- * @level: level of the Menger Sponge to draw
- */
+ * menger - Function that draws a 2D Menger Sponge
+ * @level: level of the menger sponge to draw
+ *
+ * Return: Nothing (void)
+*/
 void menger(int level)
 {
-	int i, j;
-	int dim = pow(3, level);
+	int i, j, size, h, w;
+	char sim;
 
-	for (i = 0; i < dim; i++)
+	size = pow(3, level);
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < dim; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (addSpace(i, j))
-				printf(" ");
-			else
-				printf("#");
+			h = i;
+			w = j;
+			sim = '#';
+			while (h > 0)
+			{
+				if (h % 3 == 1 && w % 3 == 1)
+					sim = ' ';
+				h /= 3;
+				w /= 3;
+			}
+			printf("%c", sim);
 		}
-		printf("");
+		printf("\n");
 	}
 }
